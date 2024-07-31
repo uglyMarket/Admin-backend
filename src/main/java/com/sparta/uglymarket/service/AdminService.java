55a06303bc00e81dcbfd8ce07aa17702c3c76a7a
JeongJae <guswjdwo3427@naver.com>
2024-08-01 00:45:10 +0900

    test : 서비스 회원가입 test 코드작성

--- src/main/java/com/sparta/uglymarket/service/AdminService.java
+++ src/main/java/com/sparta/uglymarket/service/AdminService.java
@@ -1,8 +1,8 @@
 package com.sparta.uglymarket.service;
 
-import com.sparta.uglymarket.dto.*;
+import com.sparta.uglymarket.dto.AdminRegisterRequest;
+import com.sparta.uglymarket.dto.AdminRegisterResponse;
 import com.sparta.uglymarket.entity.AdminEntity;
-import com.sparta.uglymarket.entity.RefreshToken;
 import com.sparta.uglymarket.entity.Role;
 import com.sparta.uglymarket.exception.CustomException;
 import com.sparta.uglymarket.exception.ErrorMsg;
@@ -11,11 +11,7 @@ import com.sparta.uglymarket.repository.RefreshTokenRepository;
 import com.sparta.uglymarket.util.JwtUtil;
 import com.sparta.uglymarket.util.PasswordUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -39,55 +35,5 @@ public class AdminService {
         adminEntity.setRole(Role.ROLE_ADMIN);
         adminRepository.save(adminEntity);
         return new AdminRegisterResponse("회원가입 성공!", adminEntity.getRole().name());
-    }
-
-    // 로그인
-    public ResponseEntity<AdminLoginResponse> login(AdminLoginRequest requestDto) {
-        AdminEntity admin = adminRepository.findByPhoneNumber(requestDto.getPhoneNumber())
-                .orElseThrow(() -> new CustomException(ErrorMsg.PHONE_NUMBER_NOT_FOUND));
-
-        if (!passwordUtil.matches(requestDto.getPassword(), admin.getPassword())) {
-            throw new CustomException(ErrorMsg.INVALID_PASSWORD);
-        }
-
-        String token = jwtUtil.generateAccessToken(admin.getPhoneNumber());
-        String refreshToken = jwtUtil.generateRefreshToken(admin.getPhoneNumber());
-
-        AdminLoginResponse responseDto = new AdminLoginResponse("로그인 성공!");
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token);
-        headers.add("Refresh-Token", "Bearer " + refreshToken);
-
-        return new ResponseEntity<>(responseDto, headers, HttpStatus.OK);
-    }
-
-    // 회원 정보 수정
-    public AdminUpdateResponse updateAdmin(Long id, AdminRegisterRequest adminRequest) {
-        AdminEntity admin = adminRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ErrorMsg.ADMIN_NOT_FOUND));
-
-        if (!admin.getPhoneNumber().equals(adminRequest.getPhoneNumber()) &&
-                adminRepository.findByPhoneNumber(adminRequest.getPhoneNumber()).isPresent()) {
-            throw new CustomException(ErrorMsg.DUPLICATE_PHONE_NUMBER);
-        }
-
-        admin.update(adminRequest);
-        adminRepository.save(admin);
-        return new AdminUpdateResponse("회원 정보 수정 성공!", admin.getRole().name());
-    }
-
-    // 로그아웃
-    public LogoutResponse logout(String token) {
-        String phoneNumber = jwtUtil.getPhoneNumberFromToken(token);
-        adminRepository.findByPhoneNumber(phoneNumber)
-                .orElseThrow(() -> new CustomException(ErrorMsg.PHONE_NUMBER_NOT_FOUND));
-
-        jwtUtil.revokeToken(token);
-        List<RefreshToken> refreshTokens = refreshTokenRepository.findByPhoneNumber(phoneNumber);
-        refreshTokens.forEach(refreshToken -> {
-            jwtUtil.revokeToken(refreshToken.getToken());
-        });
-
-        return new LogoutResponse("로그아웃 성공!");
     }
 }
