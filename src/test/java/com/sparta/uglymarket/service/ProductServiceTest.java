d595b15806878e5761fcb7f1088f6c1c94dedf61
JeongJae <guswjdwo3427@naver.com>
2024-08-01 01:27:04 +0900

    test : 서비스단 상품수정 test 코드작성

--- src/test/java/com/sparta/uglymarket/service/ProductServiceTest.java
+++ src/test/java/com/sparta/uglymarket/service/ProductServiceTest.java
@@ -48,4 +48,37 @@ class ProductServiceTest {
         assertEquals(productEntity, response.getProduct());
         verify(productRepository, times(1)).save(any(ProductEntity.class));
     }
+
+    @Test
+    void updateProduct() {
+        // given
+        Long id = 1L;
+        ProductUpdateRequest request = mock(ProductUpdateRequest.class);
+        ProductEntity productEntity = mock(ProductEntity.class);
+        when(productRepository.findById(eq(id))).thenReturn(Optional.of(productEntity));
+
+        doNothing().when(productEntity).updateFromRequest(any(ProductUpdateRequest.class));
+        when(productRepository.save(any(ProductEntity.class))).thenReturn(productEntity);
+
+        // when
+        ProductUpdateResponse response = productService.updateProduct(id, request);
+
+        // then
+        assertEquals("상품이 성공적으로 업데이트되었습니다.", response.getMessage());
+        assertEquals(productEntity, response.getProduct());
+        verify(productRepository, times(1)).save(productEntity);
+    }
+
+
+    @Test
+    void updateProduct_throwsException_whenProductNotFound() {
+        // given
+        Long id = 1L;
+        ProductUpdateRequest request = mock(ProductUpdateRequest.class);
+        when(productRepository.findById(eq(id))).thenReturn(Optional.empty());
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> productService.updateProduct(id, request));
+        assertEquals(ErrorMsg.PRODUCT_NOT_FOUND.getHttpStatus(), exception.getHttpStatus());
+    }
 }
