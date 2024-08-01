07f72b9d3b80c857ca56ac60c9c63930f60719d4
JeongJae <guswjdwo3427@naver.com>
2024-08-01 17:04:57 +0900

    feat : 엑세스 토큰 생성 및 전화번호 추출 test 코드 작성

--- src/test/java/com/sparta/uglymarket/UglyMarketApplicationTests.java
+++ src/test/java/com/sparta/uglymarket/UglyMarketApplicationTests.java
@@ -13,5 +13,4 @@ class UglyMarketApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
---
+++ src/test/java/com/sparta/uglymarket/util/JwtUtilTest.java
@@ -0,0 +1,39 @@
+package com.sparta.uglymarket.util;
+
+import com.sparta.uglymarket.repository.AdminRepository;
+import com.sparta.uglymarket.repository.RefreshTokenRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class JwtUtilTest {
+
+    @Mock
+    private AdminRepository adminRepository;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @InjectMocks
+    private JwtUtil jwtUtil;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        jwtUtil.init();
+    }
+
+    @Test
+    void testGenerateAccessToken() {
+        String phoneNumber = "01012345678";
+        String token = jwtUtil.generateAccessToken(phoneNumber);
+
+        assertNotNull(token);
+        assertEquals(phoneNumber, jwtUtil.getPhoneNumberFromToken(token));
+    }
+}
