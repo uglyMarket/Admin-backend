a58ea0eef7aeea4eab007f97fb80bcac415a7023
HyeonJeongJae <guswjdwo3427@naver.com>
2024-09-08 15:11:03 +0900

    refactor :OCP 원칙을 준수하도록 수정

--- src/main/java/com/sparta/uglymarket/repository/ProductRepositoryImpl.java
+++ src/main/java/com/sparta/uglymarket/repository/ProductRepositoryImpl.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepositoryJpa extends JpaRepository<ProductEntity, Long>, ProductRepository {
+public interface ProductRepositoryImpl extends JpaRepository<ProductEntity, Long>, ProductRepository {
 }
